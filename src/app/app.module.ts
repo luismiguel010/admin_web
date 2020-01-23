6f6e701df05064e1663b4401f19018638a6287f9
quemes_imac <quemesxamarin@gmail.com>
2020-01-23 07:56:08 -0500

    Add component list users and navbar on the footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersService } from './services/users.service';
 
 import { AppComponent } from './app.component';
 import { OptionsComponent } from './pages/options/options.component';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './pages/users/users.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
-
+  {path: '', redirectTo: '/users', pathMatch: 'full'},
+  {path: 'users', component: UsersComponent},
+  {path: 'options', component: OptionsComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     OptionsComponent,
+    UsersComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes ),
     FormsModule
   ],
-  providers: [],
+  providers: [
+    UsersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/users/users.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  uuidUsers  = '';
+  @Output() buttonClicked = new EventEmitter();
+
+  users: any[] = [];
+
+  constructor(protected usersService: UsersService) { }
+
+  ngOnInit() {
+    this.usersService.getUsersQuemes().subscribe(
+      (users) => this.users = users
+    );
+  }
+
+}
