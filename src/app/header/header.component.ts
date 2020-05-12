297f1cb11bbdc8a01a902b936df14f074ba27dc6
luismiguel010 <luismiguel010@gmail.com>
2020-05-12 13:02:28 -0500

    Add logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service.service';
+import { Router } from '@angular/router';
+import swal from 'sweetalert2'; 
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit() {
+  logout():void{
+    let rank = this.authService.user.rank;
+    let lastname = this.authService.user.lastname;
+    this.authService.logout();
+    swal.fire('Logout', `Bye ${rank} ${lastname}`, 'success');
+    this.router.navigate(['/login']);
   }
 
 }
