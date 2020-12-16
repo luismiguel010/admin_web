5db76bb6296edb11abc5a03d1b96465550358db1
Luis Miguel Montes Zuluaga <luismiguel010@gmail.com>
2020-12-15 20:39:47 -0500

    Refactor folders

---
+++ src/app/pages/databases-tables/imeis-list/filter-imeis.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterImeisPipe } from './filter-imeis.pipe';
+
+describe('FilterImeisPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterImeisPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pages/databases-tables/imeis-list/imeis-list.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { Imei } from './imei';
+import { ImeisListService } from '../../../services/imeis-list.service';
+import { DeleteImeiModalService } from '../../../services/delete-imei-modal.service';
+import { Router } from '@angular/router';
+import swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-imeis-list',
+  templateUrl: './imeis-list.component.html',
+  styleUrls: ['./imeis-list.component.css']
+})
+export class ImeisListComponent implements OnInit{
+
+  imeis: any[] = [];
+
+  public imei: Imei = new Imei()
+
+  constructor(protected imeiListService: ImeisListService, 
+    private imeiService: DeleteImeiModalService, private router: Router) { }
+
+  filterImei = '';
+
+  ngOnInit(): void {
+    console.log("onInit");
+    this.imeiListService.getImeiList()
+    .subscribe(
+    (imeis) => {
+      this.imeis = imeis;
+    },
+    (error) => {
+      console.error(error);
+    }
+    );
+  }
+
+  public deleteImei(imei: Imei): void{
+    this.imei = imei;
+    if(this.imei.imeiDevice == null || this.imei.imeiDevice.length == 0){
+      swal.fire('Error insert', 'Campo de imei vacío', 'error')
+      return;
+    }
+
+    this.imeiService.deleteImei(this.imei)
+    .subscribe(response => {
+        this.router.navigate(['/users'])
+        swal.fire('Imei deleted', 'Imei deleted with success','success')
+        this.ngOnInit();
+      }, err => {
+        if(err.status == 500){
+          swal.fire('Error al eliminar imei', 'Es posible que el imei no exista', 'error')
+        }
+      }
+    )
+  }
+
+}
