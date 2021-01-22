ac566a18e8011b20f8b6310fe500402f36afbd24
Luis Miguel Montes Zuluaga <luismiguel010@gmail.com>
2021-01-21 23:56:22 -0500

    Correction name in searching

--- src/app/pages/users-cards/filter-users.pipe.ts
+++ src/app/pages/users-cards/filter-users.pipe.ts
@@ -9,8 +9,8 @@ export class FilterUsersPipe implements PipeTransform {
     const resultUsers = [];
     for(const user of value){
       if(user.lastName.toLowerCase().indexOf(arg.toLowerCase()) > -1 || user.name.toLowerCase().indexOf(arg.toLowerCase()) > -1 || 
-      user.rank.toLowerCase().indexOf(arg.toLowerCase()) > -1 || user.uuidEmail.toLowerCase().indexOf(arg.toLowerCase()) > -1 || 
-      user.callSing.toLowerCase().indexOf(arg.toLowerCase()) > -1 || user.uuidJob.toLowerCase().indexOf(arg.toLowerCase()) > -1 ||
+      user.rank.toLowerCase().indexOf(arg.toLowerCase()) > -1 || user.email.toLowerCase().indexOf(arg.toLowerCase()) > -1 || 
+      user.callSing.toLowerCase().indexOf(arg.toLowerCase()) > -1 || user.job.toLowerCase().indexOf(arg.toLowerCase()) > -1 ||
       user.dependency.toLowerCase().indexOf(arg.toLowerCase()) > -1 || user.username.toLowerCase().indexOf(arg.toLowerCase()) > -1){
         resultUsers.push(user);
       };
